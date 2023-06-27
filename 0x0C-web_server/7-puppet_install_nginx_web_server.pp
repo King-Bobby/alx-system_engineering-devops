aadf9f58c145797a7d8327ce47e77a41edaab8e0
King-Bobby <noelbrowndboss@gmail.com>
2023-06-27 15:39:26 -0700

    pUppEt

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,39 @@
-# Using puppet, install and configure an Nginx server
-# also include resources in your manifest to perform a 301 redirect when querying /redirect_me.
-
+#Using puppet, install nginx and also include resources in your manifest
+# to perform a 301 redirect when querying /redirect_me.
 class nginx {
-	package { 'nginx' :
-		ensure => installed,
-	}
+    package { 'nginx':
+        ensure => installed,
+    }
 
-	service { 'nginx' :
-		ensure => running,
-		enable => true,
-		require => Package['nginx'],
-	}
+    service { 'nginx':
+        ensure => running,
+        enable => true,
+        require => Package['nginx'],
+    }
 
-	file { '/var/www/html/index.html' :
-		content => 'Hello World!',
-		require => Package['nginx'],
-	}
+    file { '/var/www/html/index.html':
+        content => 'Hello World!',
+        require => Package['nginx'],
+    }
 
-	file { '/etc/nginx/sites-available/default/' :
-		ensure => present,
-		source => 'puppet:///modules/nginx/default',
-		notify => Service['nginx'],
-	}
-}
+    file { '/etc/nginx/sites-available/default':
+        ensure => present,
+        content => "
+            server {
+                listen 80;
+                server_name _;
+                return 301 https://google.com;
+            }
+        ",
+        notify => Service['nginx'],
+    }
 
-class nginx::redirect {
-	file { '/etc/nginx/sites-available/redirect' :
-		ensure => present,
-		content => "server {\n  listen 80;\n  server_name _;\n  return 301 https://google.com;\n}\n",
-		notify => Service['nginx'],
-	}
-
-	file { '/etc/nginx/sites-enabled/redirect':
-    	ensure => link,
-		target => '/etc/nginx/sites-available/redirect',
-		require => File['/etc/nginx/sites-available/redirect'],
-		notify => Service['nginx'],
-	}
+    file { '/etc/nginx/sites-enabled/redirect':
+        ensure => link,
+        target => '/etc/nginx/sites-available/default',
+        require => File['/etc/nginx/sites-available/default'],
+        notify => Service['nginx'],
+    }
 }
 
 include nginx
-include nginx::redirect
