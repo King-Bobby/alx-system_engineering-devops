25b743e879742ce1fad51dd03095d64f270a9d12
King-Bobby <noelbrowndboss@gmail.com>
2023-08-08 16:12:40 -0700

    after

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="tmp"):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    if after != "tmp":
+        url += "?after={}".format(after) 
+    headers = {'User-Agent': 'CustomUserAgent'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title']
+            hot_list.append(title)
+
+        after = data['data']['after']
+        if not after:
+            return (hot_list)
+    else:
+        return (None)
+    return recurse(subreddit, hot_list, after)
